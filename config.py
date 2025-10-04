a143f9d4e95526200afb9ef90a3a4b5255116e1f
Stepan <steveandwonder@gmail.com>
2025-10-04 13:49:58 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+token = "Проставьте токен"
+headers = {
+    "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 YaBrowser/25.4.0.0 Safari/537.36",
+    "authorization":
+        "Bearer " + token
+}
+base_url = "https://web-gate.chitai-gorod.ru/api/v2/"
